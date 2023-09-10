3203096a9df41aeb81df0efe98085505562e65b7
AlexProt <app78@ya.ru>
2023-09-10 18:45:06 +0300

    Код

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-string[] checkarray = {"Hello","2","world",":-)","++r!"};
+  //Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+  //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+  // либо задать на старте выполнения алгоритма. 
+  //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+  
+  
+  string[] checkarray = {"Hello","2","world",":-)","+r!"};
   int chekLength=3;
   string[] outarray= new string[checkarray.Length];
   int k=0;
   for (int i=0; i<checkarray.Length; i++)
-  if (checkarray[i].Length>=chekLength) {
+  if (checkarray[i].Length<=chekLength) {
     outarray[k]=checkarray[i];
     k++;
   }
